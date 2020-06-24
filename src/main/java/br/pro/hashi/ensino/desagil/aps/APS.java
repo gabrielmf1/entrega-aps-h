92c6ebb4566d054dc6209a0aa336b5d0550c5d10
gabrielmf1 <gabrielmf1@al.insper.edu.br>
2020-06-24 13:58:23 -0300

    OO2

--- src/main/java/br/pro/hashi/ensino/desagil/aps/APS.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/APS.java
@@ -19,6 +19,7 @@ public class APS {
         model.add(new NandGate());
         model.add(new OrGate());
         model.add(new NotGate());
+        model.add(new NorGate());
 
         // Constrói a visão, que também é um controlador.
         // Juntar visão e controlador prejudica a coesão,
